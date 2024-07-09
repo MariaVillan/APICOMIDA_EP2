ccefd9c28e37ab8f4558e5b88a84aee938ea4b60
lysanderNocturn <posadanomuertos@gmail.com>
2024-07-09 00:12:40 -0600

    hostBranches1-3

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Usuario = require('../models/Usuario');
+const Usuario = require('../models/usuario');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
